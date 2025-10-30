7b14edfd5796665bfb840e262806cef8c075d20d
pythcoiner <pythcoiner@proton.me>
2025-10-30 04:10:19 +0100

    bwk-sign: re-export common structs & traits

--- sign/src/lib.rs
+++ sign/src/lib.rs
@@ -2,3 +2,8 @@ pub mod error;
 pub mod hot_signer;
 pub mod signer;
 pub mod signing_manager;
+
+pub use error::Error;
+pub use hot_signer::{HotSigner, JsonSigner};
+pub use signer::{Signer, SignerNotif};
+pub use signing_manager::SigningManager;
