7422ae356f946ceaf174128d1322c61ef3c2904b
pythcoiner <pythcoiner@proton.me>
2025-11-28 04:56:35 +0100

    bwk-sign: re-export dependencies

--- sign/src/lib.rs
+++ sign/src/lib.rs
@@ -7,3 +7,12 @@ pub use error::Error;
 pub use hot_signer::{HotSigner, JsonSigner};
 pub use signer::{Signer, SignerNotif};
 pub use signing_manager::SigningManager;
+
+// re-export
+pub use bip39;
+pub use bwk_descriptor;
+pub use bwk_keys;
+pub use bwk_utils;
+pub use miniscript;
+pub use serde;
+pub use serde_json;
